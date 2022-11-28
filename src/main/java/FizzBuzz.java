a41c6015bb4fe45db5d776eac9f28c250e456427
Hassen Gaaya <architecture.best.practices@gmail.com>
2022-11-28 23:04:43 +0100

    Tests for numbers not mutiple of neither 3 nor 5

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,12 +1,20 @@
+import java.util.Objects;
+
 public class FizzBuzz {
     public String transform(int number) {
-        if(number % 3 == 0 && number % 5 == 0)
+        if (number % 3 == 0 && number % 5 == 0)
             return "FizzBuzz";
         if (number % 3 == 0)
             return "Fizz";
-        else if (number % 5 == 0)
+        if (number % 5 == 0)
             return "Buzz";
-        else
-            return "FizzBuzz";
+        return Integer.toString(number);
+    }
+
+    public static void main(String[] args) {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(fizzBuzz.transform(i));
+        }
     }
 }
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,28 +1,36 @@
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class FizzBuzzTest {
 
+    private final FizzBuzz fizzBuzz = new FizzBuzz();
+
     @ParameterizedTest
     @ValueSource(ints = {3, 6, 9, 12})
     void returnFizzForMultipleOfThree(int number) {
-        Assertions.assertThat(new FizzBuzz().transform(number))
+        Assertions.assertThat(fizzBuzz.transform(number))
                 .isEqualTo("Fizz");
     }
 
     @ParameterizedTest
     @ValueSource(ints = {5, 10, 20, 25})
-    void returnBuzzForMultipleOfFive() {
-        Assertions.assertThat(new FizzBuzz().transform(5))
+    void returnBuzzForMultipleOfFive(int number) {
+        Assertions.assertThat(fizzBuzz.transform(number))
                 .isEqualTo("Buzz");
     }
 
     @ParameterizedTest
     @ValueSource(ints = {15, 30, 45, 60, 90})
-    void returnFizzBuzzForMultipleOfBothThreeAndFive() {
-        Assertions.assertThat(new FizzBuzz().transform(15))
+    void returnFizzBuzzForMultipleOfBothThreeAndFive(int number) {
+        Assertions.assertThat(fizzBuzz.transform(number))
                 .isEqualTo("FizzBuzz");
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 4, 14, 17, 23, 53})
+    void returnNullForNumbersNotMultipleOfNeitherThreeNorFive(int number) {
+        Assertions.assertThat(fizzBuzz.transform(number))
+                .isEqualTo(Integer.toString(number));
     }
 }
